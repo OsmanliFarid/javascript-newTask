9f5b10958484bfc01af824b67fa46e3897e3fbaf
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-19 14:54:04 +0400

    javascript-task2

---
+++ javascript-task2/app.js
@@ -0,0 +1,7 @@
+/*
+2. Üç addan ibarət massiv yaradın. Massivin ikinci elementinin dəyərini “Classified” 
+ilə əvəz edin. Massivi konsola çıxarın. */
+
+let arr = ["Murad","Ayxan","Imam"]
+arr.splice(1,1,"Classified")
+console.log(arr);
