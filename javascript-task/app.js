1691ac38d8868d9602aba736ab30285f3a87e9d9
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-19 14:53:52 +0400

    javascript-task1

---
+++ javascript-task/app.js
@@ -0,0 +1,11 @@
+/*1. Üç qrup yoldaşının adlarından ibarət massiv yaradın; Başqa bir qrup yoldaşının 
+adını massivin sonuna əlavə edin; İlk adı silin; Massivi konsola çıxarın.
+ */
+
+let arr = ["Yusif","Ehmed","Eli"]
+arr.push("murad")
+arr.shift("")
+console.log(arr);
+
+
+ 
